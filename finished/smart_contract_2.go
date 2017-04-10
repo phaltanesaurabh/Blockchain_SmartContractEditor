985141aaedcda274c1a6cc7b3e33bbe37fb184f2
Saurabh Phaltane <saurabhpradeepkumar.phaltane@sjsu.edu>
2017-04-10 00:07:46 +0000

    program

--- finished/smart_contract_2.go
+++ finished/smart_contract_2.go
@@ -1,20 +1,22 @@
 //a program to insert and update a JSON and validate with smart Contract.
 /*
-The following Code is used for insertion and parsing of json with blockchain. 
-The code inserts the smart contract schema in form of json in the blockchain and provide 
+The following Code is used for insertion and parsing of json with blockchain.
+The code inserts the smart contract schema in form of json in the blockchain and provide
 functionality to pull the json edit the json and update the json in blockchain
-this appending the block in the blockchain.       
+this appending the block in the blockchain.
 */
 
 package main
+
 import (
 	"errors"
 	"fmt"
-	"encoding/json"
-	"os"
-    "github.com/hyperledger/fabric/core/chaincode/shim"
-    "strconv"
+	//	"encoding/json"
+	//	"os"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	//  "strconv"
 )
+
 //SimpleChaincode example simple Chaincode implementation
 
 type SimpleChaincode struct {
@@ -23,7 +25,7 @@ type SimpleChaincode struct {
 //inputjson
 /*func main(){
 //data := `{"product_id":"IOT1124s","Contractid":"232241123","Parents":["Gomez","Morticia"]}`
-//Contract 
+//Contract
 //data := `{"product_id":"IOT1124s","Contractid":"232241123","stake_holders":["Saurabh_id123","Vinit_Ajay123"],"sensor_value":"24","payment_percent":"20"}`
 b := []byte(data)
 }*/
@@ -59,10 +61,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
-	}else if function == "putcontract"{
+	} else if function == "putcontract" {
 		return t.putcontract(stub, args)
-	}else if function == "validate"{
-        return t.validate(stub, args) 
+	} else if function == "validate" {
+		return t.validate(stub, args)
 	}
 
 	fmt.Println("invoke did not find func: " + function)
@@ -77,8 +79,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	// Handle different functions
 	if function == "read" { //read a variable
 		return t.read(stub, args)
-	}else if function == "getcontract"{
-        return t.getcontract(stub, args) 
+	} else if function == "getcontract" {
+		return t.getcontract(stub, args)
 	}
 
 	fmt.Println("query did not find func: " + function)
@@ -86,7 +88,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	return nil, errors.New("Received unknown function query: " + function)
 }
 
-// putcontract - Put the received bytearray smatcontract in the json 
+// putcontract - Put the received bytearray smatcontract in the json
 func (t *SimpleChaincode) putcontract(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, value string
 	var err error
@@ -124,7 +126,6 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	return nil, nil
 }
 
-
 // getcontract - Get the smart Contract from the blockchain as bytearray
 func (t *SimpleChaincode) getcontract(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
@@ -138,7 +139,6 @@ func (t *SimpleChaincode) getcontract(stub shim.ChaincodeStubInterface, args []s
 	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-
 
 		return nil, errors.New(jsonResp)
 	}
@@ -160,103 +160,99 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 
-
 		return nil, errors.New(jsonResp)
 	}
 
 	return valAsbytes, nil
 }
 
-
 // read - query function to read key/value pair
 func (t *SimpleChaincode) validate(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
-	var err,err_state,err_contract error
+	//var key, jsonResp string
+	//var err, err_state, err_contract error
 
 	if len(args) != 3 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
-    //parameters productid,contractkey,bytearray
-    //data := `{"product_id":"IOT1124s","Contractid":"232241123","stake_holders":["Saurabh_id123","Vinit_Ajay123"],"sensor_value":"24","payment_percent":"20"}`
-	
+	//parameters productid,contractkey,bytearray
+	//data := `{"product_id":"IOT1124s","Contractid":"232241123","stake_holders":["Saurabh_id123","Vinit_Ajay123"],"sensor_value":"24","payment_percent":"20"}`
+
 	/*StateJsonAsbytes := []byte(args[0])
-	contractkey :=  args[1]
-	//productid := args[2]
+		contractkey :=  args[1]
+		//productid := args[2]
 
 
-	ContractvalAsbytes, err := stub.GetState(contractkey)
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		ContractvalAsbytes, err := stub.GetState(contractkey)
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 
-		return nil, errors.New(jsonResp)
+			return nil, errors.New(jsonResp)
+		}
+
+	    var f interface{}     //Interface for marshalling the data received from blockchain contract used for comparison.
+	    var g interface{}      //Interface for receiving and marshalling the received data
+
+			err_contract = json.Unmarshal(ContractvalAsbytes, &f)
+			if (err_contract!=nil) {
+				os.Exit(1)
+			}
+
+			err_state = json.Unmarshal(StateJsonAsbytes, &g)
+			if (err_state!=nil) {
+				os.Exit(1)
+			}
+
+			contract_json := f.(map[string]interface{})
+
+		    state_json := g.(map[string]interface{})
+
+		    // The Key value iteration can be done better for dynamicity as a seperate function. to loop over the two structs.
+
+	        var sensor_value,sensor_contract string
+
+			for k, v := range contract_json {
+	    	   if k == "sensor_value" {
+
+	                    fmt.Println(k, "is to be compared", v)
+	                    sensor_value=v.(string)
+
+	                 }
+
+			}
+
+	        for k, v := range state_json {
+	    	   if k == "sensor_value" {
+
+	                    fmt.Println(k, "is to be compared", v)
+	                    sensor_contract=v.(string)
+	                 }
+
+			}
+
+
+	        val1,_ := strconv.Atoi(sensor_value)
+	        val2,_ := strconv.Atoi(sensor_contract)
+	*/
+	var exception string
+
+	if 5 < 4 {
+		exception = `{"result":"Exception: value Not acceptable","status":"failed"}`
+	} else {
+		exception = `{"result":"Success","status":"success"}`
 	}
 
-    var f interface{}     //Interface for marshalling the data received from blockchain contract used for comparison.
-    var g interface{}      //Interface for receiving and marshalling the received data 
+	exceptionAsBytes := []byte(exception)
 
-		err_contract = json.Unmarshal(ContractvalAsbytes, &f)
-		if (err_contract!=nil) {
-			os.Exit(1)
-		}
+	/*Section to validate the two jsons and put state only if data is validated*/
 
-		err_state = json.Unmarshal(StateJsonAsbytes, &g)
-		if (err_state!=nil) {
-			os.Exit(1)
-		}
+	//Smart Contract Rules :
 
-		contract_json := f.(map[string]interface{})
+	// case : blockchain.sensor_value==received.sensor_value
 
-	    state_json := g.(map[string]interface{})
+	// case : blockchain.expiry_max== received.expiry
 
-	    // The Key value iteration can be done better for dynamicity as a seperate function. to loop over the two structs. 
-
-        var sensor_value,sensor_contract string 
-
-		for k, v := range contract_json {
-    	   if k == "sensor_value" {
-                     
-                    fmt.Println(k, "is to be compared", v)
-                    sensor_value=v.(string)   
-                 
-                 }
-
-		}
-        
-        for k, v := range state_json {
-    	   if k == "sensor_value" {
-                     
-                    fmt.Println(k, "is to be compared", v)
-                    sensor_contract=v.(string) 
-                 }
-
-		}
-     
-
-        val1,_ := strconv.Atoi(sensor_value)
-        val2,_ := strconv.Atoi(sensor_contract)
-
-        var exception string*/
-
-        if ( 5 < 4 ) {
-              exception = `{"result":"Exception: value Not acceptable","status":"failed"}`
-        }else{
-              exception = `{"result":"Success","status":"success"}`
-          }
-
-		exceptionAsBytes := []byte(exception)
-
-        /*Section to validate the two jsons and put state only if data is validated*/ 
-        
-        //Smart Contract Rules :
-
-        // case : blockchain.sensor_value==received.sensor_value
-        
-        // case : blockchain.expiry_max== received.expiry
-
-        //if true : insert in to blockchain.
-    
+	//if true : insert in to blockchain.
 
 	return exceptionAsBytes, nil
 }
-
